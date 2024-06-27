0f2cad6d1546df87f1c521843779f8d6afb29c01
tonychoi3580 <tony.baekgyu.choi@gmail.com>
2024-06-27 14:26:10 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import DebtForm from "@/components/home/debtform/DebtForm";
-import DebtFormContainer from "@/components/home/DebtFormContainer";
-import { EvervaultCardDemo } from "@/components/home/EvervaultCardDemo";
-import { FaqWithImage } from "@/components/home/FaqWithImage";
-import { FlipWordsDemo } from "@/components/home/FlipWordsDemo";
-import FromTheBlog from "@/components/home/FromTheBlog";
-import { HowItWorks } from "@/components/home/HowItWorks";
-import { InfiniteMovingCardsDemo } from "@/components/home/InfiniteMovingCardsDemo";
-import { SecondHero } from "@/components/home/SecondHero";
-import { ThreeDCardDemo } from "@/components/home/ThreeDCardDemo";
-import NavBar from "@/components/nav/NavBar";
-import Image from "next/image";
+
+import DebtFormContainer from "../components/home/DebtFormContainer";
+import { FaqWithImage } from "../components/home/FaqWithImage";
+import { FlipWordsDemo } from "../components/home/FlipWordsDemo";
+import FromTheBlog from "../components/home/FromTheBlog";
+import { HowItWorks } from "../components/home/HowItWorks";
+
 
 export default function Home() {
   return (
